c427b814a7143bb39e8ea4a7b278129ddc38feec
fitorevs <142629110+fitorevs@users.noreply.github.com>
2024-04-19 12:37:20 +0200

    .

--- frontend/sanity/client.js
+++ frontend/sanity/client.js
@@ -7,12 +7,12 @@ export default createClient({
     apiVersion: '2022-03-07',
 })
 
-//skK9yr9KcybuwmlFTVqtd2MJyCGhghGpwhUt79r0DbQ9wk3sB36CsbrIojJkhwrXgBjyfAXqnjxMSOWxnCANYM49sOEV9MHUyyI2bpLe11NOtLuuNpdnqVCi3Xeozn7Bl0pKwJwQRJnlMXVr1e8oXjFa4abc3uyEvQ6AqHKpe1KzNp5mm55w
+//sk10xPSHHHbCxpVhgIpdoJYyOoPE9gU1xB50KUaOwsyq5DvDkJPCer6gEFFUjvDcQvSBhHRZCItBipmzcIEvT4pqwABhwAFEFAU3D4m71y13Cfs9YkLqbFAPp5J9BX2qesxi8nEIgumKGTyLtTWDQ7f0cgCh3bbD6Y1NK7bdMWNxpQtLn3Db
 
 export const writeClient = createClient({
     projectId: "ytmy4kup",
     dataset: "production",
     useCdn: false,
     apiVersion: "2022-03-07",
-    token: "skPBpqRkFiB4dJdpom3Nnrld2RKOerhheZfqI3U5geZc8oMYsB8wBBHPRn7Dx5eZNDkL6nvwqNy7KDupPL62scsT2dGTj9hSLVBNcIJG5Kv6g6bwsnf84PQlRpy6RHIZoDu4H3eEA6ejyFD8ozVqCuu8xT1z3e8WS5POtFwyA7OQbe4CszjB"
+    token: "sk10xPSHHHbCxpVhgIpdoJYyOoPE9gU1xB50KUaOwsyq5DvDkJPCer6gEFFUjvDcQvSBhHRZCItBipmzcIEvT4pqwABhwAFEFAU3D4m71y13Cfs9YkLqbFAPp5J9BX2qesxi8nEIgumKGTyLtTWDQ7f0cgCh3bbD6Y1NK7bdMWNxpQtLn3Db"
   })
